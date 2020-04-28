2fdbb7f7096a9986d0bbc00eefa79487b4352ee5
Dasha Nazarova <dnazz17@gmail.com>
2020-04-28 15:51:29 +0300

    Added comment

--- Venn Diagram.R
+++ Venn Diagram.R
@@ -4,4 +4,5 @@ print ("Venn Diagram")
 
 library("VennDiagram")
 
-VennDiagram::draw.triple.venn(2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"))
+## Venn Diagram with three circles of the same size
+# VennDiagram::draw.triple.venn(2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"))
