700fbfb60c75b63770be95117057352e7b8ea9b9
Dasha Nazarova <dnazz17@gmail.com>
2020-04-28 15:52:42 +0300

    Venn Diagram with three circles of different sizes

--- Venn Diagram.R
+++ Venn Diagram.R
@@ -6,3 +6,7 @@ library("VennDiagram")
 
 ## Venn Diagram with three circles of the same size
 # VennDiagram::draw.triple.venn(2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"))
+
+## Venn Diagram with three circles of different sizes
+overrideTriple=T
+VennDiagram::draw.triple.venn(area1 = 2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"),euler.d = TRUE, scaled = TRUE)
