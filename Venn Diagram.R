fae27aa40937a1f6ba74e3c26f2208284cbfe566
Dasha Nazarova <dnazz17@gmail.com>
2020-04-28 15:34:11 +0300

    Vienn diagram with three circles of the same size

--- Venn Diagram.R
+++ Venn Diagram.R
@@ -1,1 +1,7 @@
 print ("Venn Diagram")
+
+# install.packages("VennDiagram")
+
+library("VennDiagram")
+
+VennDiagram::draw.triple.venn(2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"))
