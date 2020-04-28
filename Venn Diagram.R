4eed8deba8ee2c501e08715bcf7a4def26333eec
Dasha Nazarova <dnazz17@gmail.com>
2020-04-28 17:22:36 +0300

    New Titles

--- Venn Diagram.R
+++ Venn Diagram.R
@@ -9,4 +9,4 @@ library("VennDiagram")
 
 ## Venn Diagram with three circles of different sizes
 overrideTriple=T
-VennDiagram::draw.triple.venn(area1 = 2938,2987,657,220,334,131,35,category = c("Job Sites","Official Patrol Site","Recruitment Site"),euler.d = TRUE, scaled = TRUE)
+VennDiagram::draw.triple.venn(area1 = 2938,2987,657,220,334,131,35,category = c("JS","PP","MP"),euler.d = TRUE, scaled = TRUE)
